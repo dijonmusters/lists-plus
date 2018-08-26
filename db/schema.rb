0f23fca7ce36ac58c0f67fb7987c0f16ca3e7a54
iamqaz <jonathon.d.meyers@gmail.com>
2018-08-26 21:00:58 +1000

    NEW: Add complete and delete for todo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_26_092512) do
+ActiveRecord::Schema.define(version: 2018_08_26_102353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_08_26_092512) do
     t.bigint "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
