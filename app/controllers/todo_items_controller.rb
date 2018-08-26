836fd690e8672dd82bb089ee76d2f0d01619a00d
iamqaz <jonathon.d.meyers@gmail.com>
2018-08-26 20:22:44 +1000

    NEW: Add ability to add todo item

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,28 @@
+class TodoItemsController < ApplicationController
+  before_action :set_todo_list
+  
+  def create
+    @todo_item = @todo_list.todo_items.create(todo_item_params)    
+    redirect_to @todo_list
+  end
+
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "Item successfully removed"
+    else
+      flash[:error] = "Could not remove the item"
+    end
+    redirect_to @todo_list
+  end
+
+  private
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def todo_item_params
+    params[:todo_item].permit(:content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_26_092512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "todo_items", force: :cascade do |t|
+    t.string "content"
+    t.bigint "todo_list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "todo_items", "todo_lists"
 end
